a7029f20a809c480fd8078792e9cf8a9423772e2
Mirnesa Salihovic <nessa.salihovic98@gmail.com>
2021-12-15 16:53:18 +0100

    implement backend app

--- server.js
+++ server.js
@@ -105,6 +105,6 @@ io.on('connection', (socket) => {
 
 app.get('/', (req, res) => res.send('hello!'));
 
-http.listen(3000, () => {
+http.listen(process.env.PORT || 3000, () => {
   console.log('listening on *:3000');
 });
